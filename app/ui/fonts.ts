b281c138e1ce0d8e4694a1c75d8a600427582673
zyx <1300717503@qq.com>
2024-12-27 19:55:37 +0800

    chore:first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import exp from 'constants';
+
+import { Inter,Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin']
+});
